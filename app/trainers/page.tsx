2c6a27fd71ba1b30a996edcaa7cd802c47f9e5c5
hassan34319 <mh08062@st.habib.edu.pk>
2023-10-21 21:46:04 +0500

    Added

--- app/trainers/page.tsx
+++ app/trainers/page.tsx
@@ -19,13 +19,13 @@ console.log(trainers)
     <main className="w-full h-max bg-black text-white">
       <Header />
       <div className="w-full flex flex-row justify-between items-center mt-3 md:mt-6 lg:mt-8">
-        <div className="bg-coral rounded-r-2xl w-[20%] h-[0.125rem] rounded-l-2xl  md:h-1 lg:h-2"></div>
+        <div className="bg-coral rounded-r-2xl w-[20%]  h-[0.125rem]  md:h-1 lg:h-2 "></div>
         <div
           className={`${cy.className} text-tiffany text-lg  md:text-4xl lg:text-6xl`}
         >
           GLOBAL TRAINERS
         </div>
-        <div className="bg-coral h-1 rounded-l-xl w-[20%] md:h-2 lg:h-4"></div>
+        <div className="bg-coral  h-[0.125rem] rounded-l-2xl  md:h-1 lg:h-2 w-[20%]"></div>
       </div>
       {/* Trainers */}
       <Trainer trainers={trainers} />
